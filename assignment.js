98913aa2037f37470fe0336d0ca4b93c0949ac0e
Nepal Gain <nepalgain11@gmail.com>
2021-01-22 02:58:41 +0600

    update

--- assignment.js
+++ assignment.js
@@ -5,12 +5,75 @@ function kilometerToMeter(kilometer){
         let meter = kilometer * 1000;     //kilometer to meter convert
          return meter;
     }
-    else if (kilometer == 0){
-        return "Your input number is '0', which is not countable value." // 0 is not countable number 
-    }
     else {
         return "Invalid Number." // input positive value
     } 
 }
-var result = kilometerToMeter();
+var result = kilometerToMeter(1);
 console.log(result);
+//---------------------------------------------------------------------------------
+
+// budgetCalculator
+
+function budgetCalculator(clock, phone, labtop){
+    
+    if(clock >= 1 || clock >= 0){
+       var  clockPrice = 50 * clock; // increase clock value
+    }
+    else{
+        return "Please input 'Clock' valid value"
+    }
+    if(phone >= 1 || clock >= 0){
+        var phonePrice = 100 * phone;  // increase phone  price
+     }
+    else{
+        return "Please input 'Phone' valid value"
+    }
+    if(labtop >= 1 || clock >= 0){
+        var labtopPrice = 500 * labtop;   // increase labtop price
+    }
+    else{
+        return "Please input 'Labtop' valid value"
+    }
+
+    const totalPrice = clockPrice + phonePrice + labtopPrice;
+    return totalPrice;
+
+}
+
+var result = budgetCalculator(0, 1, 1);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
